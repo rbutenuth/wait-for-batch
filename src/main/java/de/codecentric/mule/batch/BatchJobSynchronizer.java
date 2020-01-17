70e9051692f77bc8123272af6e057ee5d3d2c89b
Roger Butenuth <roger.butenuth@codecentric.de>
2020-01-17 08:54:17 +0100

    Initial state.

---
+++ src/main/java/de/codecentric/mule/batch/BatchJobSynchronizer.java
@@ -0,0 +1,99 @@
+package de.codecentric.mule.batch;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Helper to wait for a Mule Batch.
+ *
+ */
+public class BatchJobSynchronizer {
+	private static Map<String, StateHolder> jobs = new HashMap<>();
+	private static AtomicLong jobCounter = new AtomicLong(System.currentTimeMillis());
+
+	/**
+	 * Create a new "batch job" (only the administration data to wait for it).
+	 * 
+	 * @param prefix
+	 *            Prefix of the job id.
+	 * @return Unique job it, starting with <code>prefix</code>
+	 */
+	public static String createJob(String prefix) {
+		String id = prefix + String.valueOf(jobCounter.getAndIncrement());
+		synchronized (jobs) {
+			jobs.put(id, new StateHolder());
+		}
+		return id;
+	}
+
+	/**
+	 * Mark a job as finished. This should be called from within the completion
+	 * phase of a Mule batch.
+	 * 
+	 * @param id
+	 *            The <code>id</code> returned by {@link #createJob(String)}.
+	 */
+	public static void finishJob(String id, Object result) {
+		StateHolder stateHolder;
+		synchronized (jobs) {
+			stateHolder = jobs.get(id);
+		}
+		if (stateHolder != null) {
+			stateHolder.finish(result);
+		}
+	}
+
+	/**
+	 * Wait until a job finishes.
+	 * 
+	 * @param id
+	 *            The <code>id</code> returned by {@link #createJob(String)}.
+	 * @return <code>result</code> provided by {@link #finishJob(String, Object)} or
+	 *         <code>null</code when job is not found.
+	 */
+	public static Object waitUntilFinished(String id) {
+		StateHolder stateHolder;
+		Object result = null;
+		synchronized (jobs) {
+			stateHolder = jobs.get(id);
+		}
+		if (stateHolder != null) {
+			result = stateHolder.waitUntilFinished();
+			synchronized (jobs) {
+				jobs.remove(id);
+			}
+		}
+		return result;
+	}
+
+	private static class StateHolder {
+		private State state;
+		private Object result;
+
+		public StateHolder() {
+			state = State.RUNNING;
+		}
+
+		public synchronized void finish(Object result) {
+			state = State.FINISHED;
+			this.result = result;
+			notifyAll();
+		}
+
+		public synchronized Object waitUntilFinished() {
+			while (state == State.RUNNING) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					// nothing to do
+				}
+			}
+			return result;
+		}
+	}
+
+	private enum State {
+		RUNNING, FINISHED
+	}
+}
